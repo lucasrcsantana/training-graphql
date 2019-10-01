06a6adbaa61ef23c661e96cee6b4df35259eea23
Lucas Santana <lucasrcsantana@gmail.com>
2019-10-01 13:52:28 -0300

    Adjusting adicionarAtendimento Mutation

--- infraestrutura/crud/atendimento.js
+++ infraestrutura/crud/atendimento.js
@@ -17,17 +17,25 @@ class Atendimento {
     const { clienteId, petId, servicoId, status, observacoes } = item
     const data = new Date().toLocaleDateString()
 
-    const sql = `INSERT INTO Atendimentos(clienteId, petId, servicoId, data, status, observacoes) VALUES(${clienteId}, ${petId}, ${servicoId}, '${data}', '${status}', '${observacoes}')`
+    const sql = `INSERT INTO Atendimentos(clienteId, petId, servicoId, data, status, observacoes) VALUES(${clienteId}, ${petId}, ${servicoId}, '${data}', '${status}', '${observacoes}') SELECT * FROM Clientes WHERE Clientes.id = ${clienteId}; SELECT * FORM Pets Where Pets.id = ${petId}; SELECT * FROM Servicos WHERE Servicos.id = ${servicoId}`
 
-    return executaQuery(sql).then(dados => ({
-      id: dados.insertId,
-      clienteId,
-      petId,
-      servicoId,
-      data,
-      status,
-      observacoes
-    }))
+    return executaQuery(sql).then(resposta => 
+    {
+      const dados = resposta[0]
+      const cliente = resposta[1][0]
+      const pet = resposta[2][0]
+      const servico = resposta[3][0]
+
+      return ({
+        id: dados.insertId,
+        cliente,
+        pet,
+        servico,
+        data,
+        status,
+        observacoes
+    })
+  })
   }
 
   atualiza(novoItem) {
